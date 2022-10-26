340dccbff18552f4d58665de5d963488eee1bb94
Astansel <116278232+Astansel@users.noreply.github.com>
2022-10-25 22:00:38 -0400

    Update CastClass.py

--- MoviePackage/CastClass.py
+++ MoviePackage/CastClass.py
@@ -1,5 +1,25 @@
-'''
-Created on Oct 23, 2022
+CastNames = { "Shrek": 'Shrek: Mike Myers, Donkey: Eddie Murphy, Princess Fiona: Cameron Diaz, Lord Farquaad: John Lithgow',
+        "Shrek2": 'Queen: Julie Andrews, Puss In Boots: Antonio Banderas, King: John Cleese, Prince Charming: Rupert Everett',
+        "ShrekTheThird": 'Merlin: Eric Idle, Artie: Justin Timberlake, Evil Queen: Susanne Blakeslee, Pinocchio: Cody Cameron',
+        "ShrekForeverAfter": 'Brogan: John Hamm, Patrol Witch: Lake Bell, Dancing Witch: Kathy Griffin, Guard Witch: Mary Kay Place',
+        "PussInBoots": 'Puss in Boots: Antonio Banderas, Kitty Softpaws: Salma Hayek, Jack: Billy Bob Thornton, Jill: Amy Sedaris'
+            }   
 
-@author: sethapple
-'''
+class Cast():
+    
+    def CastCheck(self,name):
+        if CastNames["Shrek"] == 'Shrek: Mike Myers, Donkey: Eddie Murphy, Princess Fiona: Cameron Diaz, Lord Farquaad: John Lithgow':
+            print("These names are in the repository.")
+        else:
+            print("That name is not in the repository. Only " + str(CastNames) + " are available.")
+    
+    def __init__(self, name):
+       self.CastCheck(name)  
+       self.name = name
+    
+    def __repr__(self):
+        return "Actors = " + self.name
+    
+    def __str__(self):
+       return "The following actors were featured in this film: " + CastNames["Shrek"] in CastNames.values()
+        
